0c49ffe1c68f5c5c73a4a95d1d4b6cd743bef635
Jeon Ga Bin <hyanghope@naver.com>
2020-04-09 16:44:05 +0900

    test(api): post 데이터 test code 추가

--- test/test_api.py
+++ test/test_api.py
@@ -2,7 +2,29 @@ import pytest
 
 import json
 from . import to_json
+from main import db
+from flask import g
+from main.models.data import t_test_log
 
+
+@pytest.fixture(scope="function")
+def session():
+    session = db.session
+    g.db = session
+    yield session
+    session.rollback()
+    session.close()
+
+
+@pytest.mark.parametrize("id", [1, 5, 10, 100])
+@pytest.fixture(scope="function")
+def user(session, id):
+    session.execute(
+        t_test_log.delete().where(
+            t_test_log.c.id == id
+        )
+    )
+   
 
 # 공통 함수
 def simple_get_test_status_code(client, status_code, url, query_string=None):
@@ -18,7 +40,9 @@ def simple_post_test_status_code(client, status_code, url, body=None):
         "Accept": mimetype
     }
     rv = client.post(url, data=json.dumps(body), headers=headers)
+    print(rv.data)
     response = to_json(rv.data)
+    
     assert rv.content_type == mimetype
     assert rv.status_code == status_code
     return rv
@@ -53,4 +77,9 @@ class TestSkeleton():
   
   def test_get_skeleton_not_found(self, client):
     simple_get_test_status_code_404(client, "/api/skeleton")
-
+  
+  @pytest.mark.parametrize("id", [1, 5, 10, 100])
+  def test_post_skeleton_success(self, client, user, id):
+      data = {"id": id, "name": "test_name", "count": 10}
+      simple_post_test_status_code_200(client, "/api/skeleton", data)
+   
