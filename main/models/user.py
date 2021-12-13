abbd7cfc8ebe690a5e39614722d4ca64552c4707
jindex2411 <js.jeong@linewalks.com>
2021-12-13 17:47:29 +0900

    chore(cfg): flask_skeleton.cfg 제거

--- main/models/user.py
+++ main/models/user.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import (
     create_refresh_token,
     decode_token
 )
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from main import db
 
