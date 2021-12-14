4681a32e866e78411a3cf3584b96379b71ceec1a
jindex2411 <js.jeong@linewalks.com>
2021-12-14 16:28:43 +0900

    chore: 불필요 import 제거

--- main/__init__.py
+++ main/__init__.py
@@ -8,7 +8,6 @@ from flask import Flask
 from flask_apispec.extension import FlaskApiSpec
 from flask_compress import Compress
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
