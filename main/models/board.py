1a9ef0b6ffe30421b715f5fff6b51a3410852fae
jindex2411 <js.jeong@linewalks.com>
2021-12-14 14:43:10 +0900

    feat(db): Add Board and Reply Table

---
+++ main/models/board.py
@@ -0,0 +1,17 @@
+from sqlalchemy import func
+
+from main import db
+from main.models import schema
+from main.models import BaseTable
+
+
+class Board(BaseTable, db.Model):
+  __table_name__ = "board"
+  __table_args__ = {"schema": schema, "extend_existing": True}
+  id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+  title = db.Column(db.String(50), nullable=False) 
+  content = db.Column(db.String(1024))
+  created_time = db.Column(db.DateTime, nullable=False, server_default=func.now())
+  updated_time = db.Column(db.DateTime)
+  # 삭제 되지 않은 프로젝트 검색시 쿼리 성능 향상을 위한 index
+  deleted_time = db.Column(db.DateTime, index=True)
---
+++ main/models/reply.py
@@ -0,0 +1,17 @@
+from sqlalchemy import func
+
+from main import db
+from main.models import schema
+from main.models.board import Board
+from main.models.common import BaseTable
+
+
+class Reply(BaseTable, db.Model):
+  __table_name__ = "reply"
+  __table_args__ = {"schema": schema, "extend_existing": True}
+  # ondelete="CASCADE"를 지정하여 Board가 삭제시 자동적으로 해당된 댓글 또한 삭제
+  id = db.Column(db.Integer, db.ForeignKey(Board.id, ondelete="CASCADE"), nullable=True)
+  board_id = db.Column(db.String(50), nullable=False) 
+  comment = db.Column(db.String(256))
+  created_time = db.Column(db.DateTime, nullable=False, server_default=func.now())
+  updated_time = db.Column(db.DateTime)
