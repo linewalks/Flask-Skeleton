14dbc2466f4c98faafc71939dc2cb7f7bc20e114
jindex2411 <js.jeong@linewalks.com>
2021-12-16 17:55:16 +0900

    feat: add get comment info api

--- main/controllers/comment.py
+++ main/controllers/comment.py
@@ -2,10 +2,9 @@ from flask import Blueprint
 from flask_apispec import doc, marshal_with, use_kwargs
 
 from main.controllers.common import check_board_exisitence
-from main.models.board import Board
 from main.models.comment import Comment
 from main.models.common.error import (
-    ERROR_COMMENT_DOES_NOT_EXISTS,
+    ERROR_COMMENT_NOT_FOUND,
     ResponseError,
     SUCCESS_CREATE_COMMENT,
     SUCCESS_DELETE_COMMENT,
@@ -13,8 +12,8 @@ from main.models.common.error import (
 )
 from main.schema.comment import (
     RequestCreateComment,
-    RequestDeleteComment,
-    RequestUpdateComment
+    RequestUpdateComment,
+    ResponseCommentInfo
 )
 
 
@@ -39,13 +38,35 @@ def create_comment(board, content):
   return SUCCESS_CREATE_COMMENT.get_response()
 
 
-@comment_bp.route("/<int:board_id>/update", methods=["POST"])
+@comment_bp.route("/<int:board_id>/<int:comment_id>", methods=["GET"])
+@marshal_with(ResponseCommentInfo, code=200)
+@marshal_with(ResponseError)
+@doc(
+    tags=[API_CATEGORY],
+    summary="게시판 댓글 정보",
+    description="해당 게시판의 댓글을 정보를 확인 합니다."
+)
+@check_board_exisitence
+def get_comment_info(board, comment_id):
+  comment = Comment.get(
+      comment_id,
+      board.id
+  )
+  if not comment:
+    return ERROR_COMMENT_NOT_FOUND.get_response()
+
+  return {
+      "comment_info": comment.as_dict()
+  }
+
+
+@comment_bp.route("/<int:board_id>/<int:comment_id>", methods=["POST"])
 @use_kwargs(RequestUpdateComment)
 @marshal_with(ResponseError)
 @doc(
     tags=[API_CATEGORY],
-    summary="게시판 댓글 생성",
-    description="해당 게시판의 댓글을 생성 합니다."
+    summary="게시판 댓글 수정",
+    description="해당 게시판의 댓글을 수정 합니다."
 )
 @check_board_exisitence
 def update_comment(board, comment_id, content):
@@ -54,18 +75,17 @@ def update_comment(board, comment_id, content):
       board.id
   )
   if not comment:
-    return ERROR_COMMENT_DOES_NOT_EXISTS.get_response()
+    return ERROR_COMMENT_NOT_FOUND.get_response()
   comment.update(content)
   return SUCCESS_UPDATE_COMMENT.get_response()
 
 
-@comment_bp.route("/<int:board_id>/delete", methods=["DELETE"])
-@use_kwargs(RequestDeleteComment)
+@comment_bp.route("/<int:board_id>/<int:comment_id>", methods=["DELETE"])
 @marshal_with(ResponseError)
 @doc(
     tags=[API_CATEGORY],
-    summary="게시판 댓글 생성",
-    description="해당 게시판의 댓글을 생성 합니다."
+    summary="게시판 댓글 삭제",
+    description="해당 게시판의 댓글을 삭제 합니다."
 )
 @check_board_exisitence
 def delete_comment(board, comment_id):
@@ -74,6 +94,6 @@ def delete_comment(board, comment_id):
       board.id
   )
   if not comment:
-    return ERROR_COMMENT_DOES_NOT_EXISTS.get_response()
+    return ERROR_COMMENT_NOT_FOUND.get_response()
   Comment.delete(comment)
   return SUCCESS_DELETE_COMMENT.get_response()
--- main/schema/comment.py
+++ main/schema/comment.py
@@ -2,21 +2,30 @@ from marshmallow import fields, Schema
 
 
 # Schema
-class CommentSchema(Schema):
+class CommentContentSchema(Schema):
   content = fields.Str(requried=True)
 
 
+class CommentSchema(CommentContentSchema):
+  id = fields.Int(required=True)
+  board_id = fields.Int(required=True, data_key="boardId")
+  created_time = fields.DateTime(required=True, data_key="createdTime")
+  updated_time = fields.DateTime(required=True, allow_none=True, data_key="updatedTime")
+
+
 # Request
-class RequestCreateComment(CommentSchema):
+class RequestCreateComment(CommentContentSchema):
   pass
 
 
-class RequestUpdateComment(CommentSchema):
-  comment_id = fields.Int(reqiroed=True)
-
-
-class RequestDeleteComment(Schema):
-  comment_id = fields.Int(reqiroed=True)
+class RequestUpdateComment(CommentContentSchema):
+  pass
 
 
 # Response
+class ResponseCommentInfo(Schema):
+  comment_info = fields.Nested(
+      CommentSchema,
+      required=True,
+      data_key="commentInfo"
+  )
